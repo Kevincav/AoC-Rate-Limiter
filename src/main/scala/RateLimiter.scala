6b961aaf728d5d402244c53c7e09790bdddb33b6
kevinmelkowski <kevin.melkowski@gmail.com>
2025-04-17 10:09:42 -0600

    Updated execute code block to allow for curl call passed in from AoC Solver

--- src/main/scala/RateLimiter.scala
+++ src/main/scala/RateLimiter.scala
@@ -9,24 +9,24 @@ import scala.concurrent.duration.*
 
 case class RateLimiter(year: Int, day: Int)(implicit databaseDetails: DatabaseDetails = DatabaseDetails()) {
   def check(part: Int): IO[Result] =
-      RateLimiterDAO().findByPrimaryKey(year, day, part).map {
-        case Left(_) => Result(Status.ERROR, None, None, None)
-        case Right(value) => value match {
-          case None => Result(Status.NO_RESULTS, None, None, None)
-          case Some(row) if row.isSolved => Result(Status.SOLVED, None, Some(row.answer), Some(row.isSolved))
-          case Some(row) if Deadline(row.updateTime).hasTimeLeft() =>
-            Result(Status.WAITING, Some(Deadline(row.updateTime).timeLeft), Some(row.answer), Some(row.isSolved))
-          case _ => Result(Status.READY, None, None, None)
-        }
+    RateLimiterDAO().findByPrimaryKey(year, day, part).map {
+      case Left(_) => Result(Status.ERROR, None, None, None)
+      case Right(value) => value match {
+        case None => Result(Status.NO_RESULTS, None, None, None)
+        case Some(row) if row.isSolved => Result(Status.SOLVED, None, Some(row.answer), Some(row.isSolved))
+        case Some(row) if Deadline(row.updateTime).hasTimeLeft() =>
+          Result(Status.WAITING, Some(Deadline(row.updateTime).timeLeft), Some(row.answer), Some(row.isSolved))
+        case _ => Result(Status.READY, None, None, None)
       }
+    }
 
-  def execute(part: Int)(codeBlock: => IO [(Any, Boolean)]): IO[Result] =
+  def execute(answer: Any, part: Int)(codeBlock: (Any, Int) => IO[Boolean]): IO[Result] =
     RateLimiterDAO().createTable.flatMap { _ =>
       check(part).flatMap {
         case Result(status, deadline, answer, isSolved) if status != Status.READY && status != Status.NO_RESULTS =>
           IO.pure(Result(status, deadline, answer, isSolved))
         case _ => for {
-          (answer, isSolved) <- codeBlock
+          isSolved <- codeBlock(answer, part)
           _ <- RateLimiterDAO().insertOrReplace(year, day, part, isSolved, answer, databaseDetails.timeRemaining)
         } yield Result(Status.SUCCESS, Some(Deadline(databaseDetails.timeRemaining).timeLeft), Some(answer), Some(isSolved))
       }
--- src/test/scala/RateLimiterTest.scala
+++ src/test/scala/RateLimiterTest.scala
@@ -10,21 +10,20 @@ import cats.effect.IO
 import java.nio.file.Files
 import scala.concurrent.duration.*
 
-def getAnswer(answer: Long): Long = answer
-def isSolved(isSolved: Boolean): Boolean = isSolved
+def isSolved(isSolved: Any, part: Int): IO[Boolean] = IO.pure(isSolved.toString == "true")
 
 class RateLimiterTest extends AnyFunSuite with Matchers {
   test("Test executing first and second time < 5 min") {
     val databasePath = Files.createTempFile("sqlite.database.", ".db")
     implicit val databaseDetails: DatabaseDetails = DatabaseDetails("unit_test", s"jdbc:sqlite:$databasePath")
 
-    val result = RateLimiter(2000, 1).execute(1)(IO.pure((getAnswer(1L), isSolved(false)))).unsafeRunSync()
+    val result = RateLimiter(2000, 1).execute(false, 1)(isSolved).unsafeRunSync()
     assert(result.status == Status.SUCCESS)
     assert(!result.isSolved.get)
-    assert(result.answer.get == 1L)
+    assert(result.answer.get == false)
     assert(result.timeRemaining.get.toMinutes == 5.minutes.fromNow.timeLeft.toMinutes)
 
-    val result2 = RateLimiter(2000, 1).execute(1)(IO.pure((getAnswer(1L), isSolved(false)))).unsafeRunSync()
+    val result2 = RateLimiter(2000, 1).execute(false, 1)(isSolved).unsafeRunSync()
     assert(result2.status == Status.WAITING)
     assert(result2.timeRemaining.get.toMinutes == 5.minutes.fromNow.timeLeft.toMinutes)
 
@@ -35,16 +34,16 @@ class RateLimiterTest extends AnyFunSuite with Matchers {
     val databasePath = Files.createTempFile("sqlite.database.", ".db")
     implicit val databaseDetails: DatabaseDetails = DatabaseDetails("unit_test", s"jdbc:sqlite:$databasePath")
 
-    val result = RateLimiter(2000, 1).execute(1)(IO.pure((getAnswer(1L), isSolved(true)))).unsafeRunSync()
+    val result = RateLimiter(2000, 1).execute(true, 1)(isSolved).unsafeRunSync()
     assert(result.status == Status.SUCCESS)
     assert(result.isSolved.get)
-    assert(result.answer.get == 1L)
+    assert(result.answer.get == true)
     assert(result.timeRemaining.get.toMinutes == 5.minutes.fromNow.timeLeft.toMinutes)
 
-    val result2 = RateLimiter(2000, 1).execute(1)(IO.pure((getAnswer(2L), isSolved(false)))).unsafeRunSync()
+    val result2 = RateLimiter(2000, 1).execute(false, 1)(isSolved).unsafeRunSync()
     assert(result2.status == Status.SOLVED)
     assert(result.isSolved.get)
-    assert(result.answer.get == 1L)
+    assert(result.answer.get == true)
     assert(result2.timeRemaining.isEmpty)
 
     Files.deleteIfExists(databasePath)
@@ -54,16 +53,16 @@ class RateLimiterTest extends AnyFunSuite with Matchers {
     val databasePath = Files.createTempFile("sqlite.database.", ".db")
     implicit val databaseDetails: DatabaseDetails = DatabaseDetails("unit_test", s"jdbc:sqlite:$databasePath", 0.minutes.fromNow.time)
 
-    val result = RateLimiter(2000, 1).execute(1)(IO.pure((getAnswer(1L), isSolved(false)))).unsafeRunSync()
+    val result = RateLimiter(2000, 1).execute(false, 1)(isSolved).unsafeRunSync()
     assert(result.status == Status.SUCCESS)
     assert(!result.isSolved.get)
-    assert(result.answer.get == 1L)
+    assert(result.answer.get == false)
     assert(result.timeRemaining.get.toMinutes == 0.minutes.fromNow.timeLeft.toMinutes)
 
-    val result2 = RateLimiter(2000, 1).execute(1)(IO.pure((getAnswer(2L), isSolved(false)))).unsafeRunSync()
+    val result2 = RateLimiter(2000, 1).execute(true, 1)(isSolved).unsafeRunSync()
     assert(result2.status == Status.SUCCESS)
-    assert(!result2.isSolved.get)
-    assert(result2.answer.get == 2L)
+    assert(result2.isSolved.get)
+    assert(result2.answer.get == true)
     assert(result2.timeRemaining.get.toMinutes == 0.minutes.fromNow.timeLeft.toMinutes)
 
     Files.deleteIfExists(databasePath)
@@ -76,7 +75,7 @@ class RateLimiterTest extends AnyFunSuite with Matchers {
     val resultError = RateLimiter(2000, 1).check(1).unsafeRunSync()
     assert(resultError.status == Status.ERROR)
 
-    RateLimiter(2000, 1).execute(2)(IO.pure((getAnswer(1L), isSolved(false)))).unsafeRunSync()
+    val result = RateLimiter(2000, 1).execute(false, 2)(isSolved).unsafeRunSync()
     assert(RateLimiter(2000, 1).check(1).unsafeRunSync().status == Status.NO_RESULTS)
     assert(RateLimiter(2000, 1).check(2).unsafeRunSync().status == Status.READY)
 
@@ -87,8 +86,8 @@ class RateLimiterTest extends AnyFunSuite with Matchers {
     val databasePath = Files.createTempFile("sqlite.database.", ".db")
     implicit val databaseDetails: DatabaseDetails = DatabaseDetails("unit_test", s"jdbc:sqlite:$databasePath")
 
-    RateLimiter(2000, 1).execute(1)(IO.pure((getAnswer(1L), isSolved(false)))).unsafeRunSync()
-    RateLimiter(2000, 1).execute(2)(IO.pure((getAnswer(1L), isSolved(true)))).unsafeRunSync()
+    RateLimiter(2000, 1).execute(false, 1)(isSolved).unsafeRunSync()
+    RateLimiter(2000, 1).execute(true, 2)(isSolved).unsafeRunSync()
 
     assert(RateLimiter(2000, 1).check(1).unsafeRunSync().status == Status.WAITING)
     assert(RateLimiter(2000, 1).check(2).unsafeRunSync().status == Status.SOLVED)
